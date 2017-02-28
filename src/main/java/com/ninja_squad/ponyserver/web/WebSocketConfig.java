297d57a3f3b865b6e8b89a25c668189e395223c5
bertrandlf <bertrandlf@gmail.com>
2017-02-28 15:48:19 +0100

    simplify websocket usage

--- src/main/java/com/ninja_squad/ponyserver/web/WebSocketConfig.java
+++ src/main/java/com/ninja_squad/ponyserver/web/WebSocketConfig.java
@@ -1,17 +1,11 @@
 package com.ninja_squad.ponyserver.web;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.converter.MessageConverter;
-import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
-import org.springframework.messaging.handler.invocation.HandlerMethodReturnValueHandler;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+
 
 /**
  * The Spring configuration of web sockets
@@ -19,7 +13,7 @@ import org.springframework.web.socket.config.annotation.WebSocketTransportRegist
  */
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     /**
      * Configures the websocket message broker with the path "/topic". The positions of every pony during a running
@@ -29,6 +23,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/race");
+        registry.setApplicationDestinationPrefixes("/api");
     }
 
     /**
@@ -38,33 +33,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
-    }
-
-    @Override
-    public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
-
-    }
-
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-    }
-
-    @Override
-    public void configureClientOutboundChannel(ChannelRegistration registration) {
-    }
-
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-
-    }
-
-    @Override
-    public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> returnValueHandlers) {
-
-    }
-
-    @Override
-    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
-        return true;
     }
 }
